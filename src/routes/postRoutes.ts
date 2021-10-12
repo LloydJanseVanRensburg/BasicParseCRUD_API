a1e53f2e9a7bd615b79aa6a03f968ff3b5e9ec9f
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-10-12 19:57:46 +0200

    woring on swagger docs

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -4,88 +4,65 @@ import restify from 'restify';
 module.exports = (server: restify.Server) => {
   /**
    * @swagger
-   * tags:
-   *  name: Posts
-   *  description: The Posts Managing API
+   * components:
+   *   schemas:
+   *     Post:
+   *       type: object
+   *       required:
+   *         - title
+   *         - content
+   *       properties:
+   *         objectId:
+   *           type: string
+   *           description: The auto-generated id of the post
+   *         title:
+   *           type: string
+   *           description: The post title
+   *         content:
+   *           type: string
+   *           description: The post content
+   *       example:
+   *         objectId: d5fE_asz
+   *         title: hello world title
+   *         content: lorem ipsum ltsoe rivc cis lorem ipsum ltsoe rivc cis
    */
 
   /**
    * @swagger
-   * /api/v1/posts/:
-   *  get:
-   *    summary: Returns list of all posts
-   *    tags: [Posts]
-   *    responses:
-   *      200:
-   *        description: The list of the posts
-   *      400:
-   *        description: Bad Request
-   *      500:
-   *        description: Internal Server Error
+   * tags:
+   *   name: Posts
+   *   description: The posts managing API
    */
+
   server.get('/api/v1/posts', postControllers.getAllPosts);
 
   /**
    * @swagger
-   * /api/v1/posts/:
-   *  post:
-   *    summary: Create a new post in database
-   *    tags: [Posts]
-   *    responses:
-   *      200:
-   *        description: Newly created post
-   *      400:
-   *        description: Bad Request
-   *      500:
-   *        description: Internal Server Error
+   * /api/v1/posts:
+   *   post:
+   *     summary: Create a new post
+   *     tags: [Posts]
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/Post'
+   *     responses:
+   *       200:
+   *         description: The post was successfully created
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/Post'
+   *       500:
+   *         description: Some server error
    */
   server.post('/api/v1/posts', postControllers.createNewPost);
 
-  /**
-   * @swagger
-   * /api/v1/posts/{postId}:
-   *  get:
-   *    summary: Returns the found post by id
-   *    tags: [Posts]
-   *    responses:
-   *      200:
-   *        description: Found Post Object
-   *      400:
-   *        description: Bad Request
-   *      500:
-   *        description: Internal Server Error
-   */
   server.get('/api/v1/posts/:id', postControllers.getPostById);
 
-  /**
-   * @swagger
-   * /api/v1/posts/{postId}:
-   *  put:
-   *    summary: Returns the update version of post
-   *    tags: [Posts]
-   *    responses:
-   *      201:
-   *        description: Post successfully updated
-   *      400:
-   *        description: Bad Request
-   *      500:
-   *        description: Internal Server Error
-   */
   server.put('/api/v1/posts/:id', postControllers.updatePostById);
 
-  /**
-   * @swagger
-   * /api/v1/posts/{postId}:
-   *  delete:
-   *    summary: Delete the post with id
-   *    tags: [Users]
-   *    responses:
-   *      203:
-   *        description: Successfully delete post
-   *      400:
-   *        description: Bad Request
-   *      500:
-   *        description: Internal Server Error
-   */
   server.del('/api/v1/posts/:id', postControllers.deletePostById);
 };
